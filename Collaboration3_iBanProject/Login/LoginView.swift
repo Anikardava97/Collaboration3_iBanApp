d57fd26616cfc945b7e8a7a532c12e5fac65591a
Tparuna1 <145837277+Tparuna1@users.noreply.github.com>
2024-01-10 20:30:20 +0400

    Create UI For Login Page and Sign up page
    create checkmarks for password

--- Collaboration3_iBanProject/Login/LoginView.swift
+++ Collaboration3_iBanProject/Login/LoginView.swift
@@ -6,3 +6,103 @@
 //
 
 import SwiftUI
+
+
+struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Image("logoWithTitle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150, height: 150)
+                .padding(.bottom, 60)
+            
+            EmailTextField(email: $email)
+        
+            PasswordSecureField(password: $password)
+            
+            HStack {
+                Text("Don't have an account?")
+                    .foregroundColor(.white)
+                
+                Button(action: {
+                    // Action for signup button
+                }) {
+                    Text("Sign Up")
+                        .foregroundColor(.customAccentColor)
+                }
+            }
+            .padding(.top, 20)
+            
+            Spacer()
+            LoginButton()
+            
+            Spacer()
+        }
+        .background(Color.customBackgroundColor)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct EmailTextField: View {
+    @Binding var email: String
+    
+    var body: some View {
+        HStack {
+            Text("Email")
+                .foregroundColor(.white)
+            Spacer()
+        }
+        .padding(.horizontal)
+        TextField("Enter your email address", text: $email)
+            .padding()
+            .background(Color.customTextFieldColor)
+            .tint(.white)
+            .cornerRadius(8.0)
+            .padding(.horizontal)
+    }
+}
+
+struct PasswordSecureField: View {
+    @Binding var password: String
+    
+    var body: some View {
+        HStack {
+            Text("Password")
+                .foregroundColor(.white)
+            Spacer()
+        }
+        .padding(.horizontal)
+        
+        SecureField("Enter your password", text: $password)
+            .padding()
+            .background(Color.customTextFieldColor)
+            .cornerRadius(8.0)
+            .padding(.horizontal)
+    }
+}
+
+struct LoginButton: View {
+    var body: some View {
+        Button(action: {
+            // Action for login button
+        }) {
+            Text("Log In")
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.customAccentColor)
+                .cornerRadius(8.0)
+                .padding(.horizontal)
+        }
+    }
+}
+
+
+#Preview {
+    LoginView()
+}
--- Collaboration3_iBanProject/SignUp/SignUpView.swift
+++ Collaboration3_iBanProject/SignUp/SignUpView.swift
@@ -6,3 +6,117 @@
 //
 
 import SwiftUI
+
+
+struct SignUpView: View {
+    @State private var fullName: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isSignUpEnabled: Bool = false
+    @State private var isMinLengthMet: Bool = false
+    @State private var isCapitalLetterMet: Bool = false
+    @State private var isNumberMet: Bool = false
+    @State private var isUniqueCharacterMet: Bool = false
+    
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Image("logoWithTitle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150, height: 150)
+                .padding(.bottom, 60)
+                        
+            EmailTextField(email: $email)
+            
+            PasswordSecureField(password: $password)
+            
+            PasswordStrengthChecklist(
+                isMinLengthMet: isMinLengthMet,
+                isCapitalLetterMet: isCapitalLetterMet,
+                isNumberMet: isNumberMet,
+                isUniqueCharacterMet: isUniqueCharacterMet
+            )
+            
+            Spacer()
+            SignUpButton(isEnabled: isSignUpEnabled)
+    
+            Spacer()
+        }
+        .background(Color.customBackgroundColor)
+        .edgesIgnoringSafeArea(.all)
+        .onChange(of: password) { newValue in
+            isMinLengthMet = newValue.count >= 8
+            isCapitalLetterMet = newValue.rangeOfCharacter(from: .uppercaseLetters) != nil
+            isNumberMet = newValue.rangeOfCharacter(from: .decimalDigits) != nil
+            isUniqueCharacterMet = newValue.rangeOfCharacter(from: CharacterSet(charactersIn: "@#$%^&*")) != nil
+            
+            isSignUpEnabled = isMinLengthMet && isCapitalLetterMet && isNumberMet && isUniqueCharacterMet
+        }
+    }
+}
+
+
+struct SignUpButton: View {
+    var isEnabled: Bool
+    
+    var body: some View {
+        Button(action: {
+            // Action for sign up button
+        }) {
+            Text("Sign Up")
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(isEnabled ? Color.customAccentColor : Color.gray)
+                .cornerRadius(8.0)
+                .padding(.horizontal)
+                .opacity(isEnabled ? 1.0 : 0.5)
+                .disabled(!isEnabled)
+        }
+    }
+}
+
+struct PasswordStrengthChecklist: View {
+    var isMinLengthMet: Bool
+    var isCapitalLetterMet: Bool
+    var isNumberMet: Bool
+    var isUniqueCharacterMet: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Your password should have:")
+                .foregroundColor(.white)
+            
+            ChecklistItem(title: "At least 8 characters", isMet: isMinLengthMet)
+            ChecklistItem(title: "At least one capital letter", isMet: isCapitalLetterMet)
+            ChecklistItem(title: "At least one number", isMet: isNumberMet)
+            ChecklistItem(title: "At least one unique character", isMet: isUniqueCharacterMet)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal)
+        .frame(maxWidth: .infinity, alignment: .leading)
+
+    }
+}
+
+struct ChecklistItem: View {
+    var title: String
+    var isMet: Bool
+    
+    var body: some View {
+        HStack(spacing: 5) {
+            Image(systemName: isMet ? "checkmark" : "checkmark")
+                .foregroundColor(isMet ? .green : .gray)
+            Text(title)
+                .foregroundColor(.gray)
+                .font(.system(size: 14))
+        }
+    }
+}
+
+
+#Preview {
+    SignUpView()
+}
